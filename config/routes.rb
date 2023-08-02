1a4ba4efa9bc24ed66349860d30fc8c330502781
CloneyMari <cloneymaricarandang@gmail.com>
2023-08-02 15:29:58 +0800

    [feature 1 & 2] add separate log in for admin and client

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'home#index'
+  constraints(ClientDomainConstraint.new) do
+    scope module: :client do
+      devise_for :users, controllers: {
+        registrations: 'client/registrations',
+        sessions: 'client/sessions'
+      }
+      root 'home#index', as: :client_root
+    end
+  end
+
+  constraints(AdminDomainConstraint.new) do
+    namespace 'admin', path: '' do
+      devise_for :users, controllers: {
+        sessions: 'admin/sessions'
+      }, as: :admin
+      root 'home#index', as: :admin_root
+    end
+  end
 end
