99a3a26794df749575cfc8aad85fb4b99ec13d67
CloneyMari <cloneymaricarandang@gmail.com>
2023-08-08 13:43:44 +0800

    add image column and uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_31_074611) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_100502) do
+  create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "city_id"
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_address_barangays_on_city_id"
+  end
+
+  create_table "address_cities", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "province_id"
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["province_id"], name: "index_address_cities_on_province_id"
+  end
+
+  create_table "address_provinces", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "region_id"
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_address_provinces_on_region_id"
+  end
+
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_074611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
